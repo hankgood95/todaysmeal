e07e62b6d6c7037d5837251ff3015ca3664d6d66
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-04-03 01:44:28 +0900

    css error

--- TM/src/main/webapp/resources/vote/js/button.js
+++ TM/src/main/webapp/resources/vote/js/button.js
@@ -1,13 +1,31 @@
 function agree() {
-	if (confirm("찬성하시겠습니까?"))
+	if (confirm("찬성하시겠습니까?")) {
 		alert("확인 눌렀다~~~~");
+		$.ajax ({
+			type: "POST",
+			url: "recordVote",
+			data: data,
+			success: function(data) {
+				console.log("success");
+			}
+		});
+	}
 	else
 		alert("취소 눌렀다~~~~");
 	return ;
 }
 function disagree() {
-	if (confirm("반대하시겠습니까?"))
+	if (confirm("반대하시겠습니까?")) {
 		alert("확인 눌렀다~~~~");
+		$.ajax ({
+			type: "POST",
+			url: "recordVote",
+			data: data,
+			success: function(data) {
+				console.log(data);
+			}
+		});
+	}
 	else
 		alert("취소 눌렀다~~~~");
 	return ;
